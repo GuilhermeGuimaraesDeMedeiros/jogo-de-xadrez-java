1e13a2b091376d34f9a3328490f592846c30a60b
Guilherme Guimarães de Medeiros <gg.medeiros2011@gmail.com>
2023-06-26 17:56:21 -0300

    Pawn

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -34,7 +34,7 @@ public abstract class ChessPiece extends Piece{
 		return ChessPosition.fromPosition(position);
 	}
 
-	protected boolean isThereoponentPiece(Position position) {
+	protected boolean isThereOponentPiece(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
